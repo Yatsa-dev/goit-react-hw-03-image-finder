61385f592d6fba54addce06707bad1c3fe345b68
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-10-17 19:56:22 +0300

    clear app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-// import "./App.css";
-import Profile from 'components/Profile';
-import Statistics from 'components/Statistics';
-import FriendList from 'components/FriendList';
-import TransactionHistory from 'components/TransactionHistory';
+
 import Container from 'components/Container';
 
-import user from './API/user.json';
-import statisticalData from './API/statistical-data.json';
-import friends from './API/friends.json';
-import transactions from './API/transactions.json';
+
 
 function App() {
   return (
     <Container>
-      <Profile
-        avatar={user.avatar}
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+     
     </Container>
   );
 }
